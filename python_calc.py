dfa898d82bf54240bee1c00aa77ab359900ae5ed
Kulichok26 <katyusha.kulikova.2005@mail.ru>
2024-12-15 16:31:46 +0300

    Fixed help_form текущие ошибки:     --исправить операции с одним числом     --оптимизация

--- python_calc.py
+++ python_calc.py
@@ -2,6 +2,13 @@ from tkinter import *
 from math import *
 from time import sleep
 
+def help_form(event=None):
+    help_form = Toplevel(window)
+    help_form.title('Справка')
+    help_form.geometry("1000x300")
+    label = Label(help_form, text="Спасибо моей любимой матери Куликовой Наталии Владимировнеза то, что всегда была со мной. Спасибо отцу Куликову Сергею Вячеславовичу за то, что показал мир")
+    label.place(x = 20, y = 20)
+    
 def on_key_press(event):
     if event.char not in '0123456789':
         return "break"
@@ -12,8 +19,6 @@ def input_into_entry(symbol, op = None, tag = 'notnull'):
     global op_count
     global for_empty_massiv
 
-    #if tag_last == 'empty':
-        #print("!")
     for i in range(10, 17): #18 -> 17
         if (for_empty_massiv[i] == (entry.get() + symbol)):
             return
@@ -88,7 +93,7 @@ def count_result():
         if float(text[:-2])**2==int(float(text[:-2])**2):
             entry.insert(0, int(float(text[:-2])**2))
         else:
-            entry.insert(0, round((float(text[:-2]))**2), 4)
+            entry.insert(0, round((float(text[:-2]))**2, 4))
     elif operation == 'sqrt':
         clear_all()
         if sqrt(float(text[:-6]))==int(sqrt(float(text[:-6]))):
@@ -110,7 +115,6 @@ def count_result():
 
 button_values_massiv = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', 'x', '/', '^(1/2)', '^(-1)', '^2']
 global tag_last
-#tag_last = 'empty'
 global op_count
 op_count = 0
 global for_empty_massiv
@@ -120,70 +124,74 @@ open_massiv = ['++', '--', 'xx', '//', '+-', '+x', '+/', '-+', '-x', '-/', 'x+',
 
 window = Tk()
 window.title('Калькулятор')
-window.geometry('600x600+700+100')
+window.geometry('220x350+700+100')
+window.bind('<Shift-F11>', help_form)
 
 entry = Entry(window, width = 13, font = ('', 20))
-entry.place(x = 100, y = 10)
+entry.place(x = 10, y = 10)
 entry.bind("<KeyPress>", on_key_press)
 
+button_help = Button(window, text="Справка     Shift-F11", command = help_form)
+button_help.place(x = 10, y = 310, width = 200, height = 30)
+
 button1 = Button(window, bg = 'black', fg = 'white', text = '7', command = lambda: input_into_entry('7'))
-button1.place(x = 100, y = 100, width = 50, height = 50)
+button1.place(x = 10, y = 100, width = 50, height = 50)
 
 button2 = Button(window, bg = 'black', fg = 'white', text = '8', command = lambda: input_into_entry('8'))
-button2.place(x = 150, y = 100, width = 50, height = 50)
+button2.place(x = 60, y = 100, width = 50, height = 50)
 
 button3 = Button(window, bg = 'black', fg = 'white', text = '9', command = lambda: input_into_entry('9'))
-button3.place(x = 200, y = 100, width = 50, height = 50)
+button3.place(x = 110, y = 100, width = 50, height = 50)
 
 button4 = Button(window, bg = 'black', fg = 'white', text = '4', command = lambda: input_into_entry('4'))
-button4.place(x = 100, y = 150, width = 50, height = 50)
+button4.place(x = 10, y = 150, width = 50, height = 50)
 
 button5 = Button(window, bg = 'black', fg = 'white', text = '5', command = lambda: input_into_entry('5'))
-button5.place(x = 150, y = 150, width = 50, height = 50)
+button5.place(x = 60, y = 150, width = 50, height = 50)
 
 button6 = Button(window, bg = 'black', fg = 'white', text = '6', command = lambda: input_into_entry('6'))
-button6.place(x = 200, y = 150, width = 50, height = 50)
+button6.place(x = 110, y = 150, width = 50, height = 50)
 
 button7 = Button(window, bg = 'black', fg = 'white', text = '1', command = lambda: input_into_entry('1'))
-button7.place(x = 100, y = 200, width = 50, height = 50)
+button7.place(x = 10, y = 200, width = 50, height = 50)
 
 button8 = Button(window, bg = 'black', fg = 'white', text = '2', command = lambda: input_into_entry('2'))
-button8.place(x = 150, y = 200, width = 50, height = 50)
+button8.place(x = 60, y = 200, width = 50, height = 50)
 
 button9 = Button(window, bg = 'black', fg = 'white', text = '3', command = lambda: input_into_entry('3'))
-button9.place(x = 200, y = 200, width = 50, height = 50)
+button9.place(x = 110, y = 200, width = 50, height = 50)
 
 button0 = Button(window, bg = 'black', fg = 'white', text = '0', command = lambda: input_into_entry('0', None)) #no 'empty'
-button0.place(x = 150, y = 250, width = 50, height = 50)
+button0.place(x = 60, y = 250, width = 50, height = 50)
 
 button_clear_all = Button(window, bg = 'black', fg = 'white', text = 'C', command = clear_all)
-button_clear_all.place(x = 100, y = 250, width = 50, height = 50)
+button_clear_all.place(x = 10, y = 250, width = 50, height = 50)
 
 button_dot = Button(window, bg = 'black', fg = 'white', text = '.', command = lambda: input_into_entry('.'))
-button_dot.place(x = 200, y = 250, width = 50, height = 50)
+button_dot.place(x = 110, y = 250, width = 50, height = 50)
 
 button_plus = Button(window, bg = 'black', fg = 'white', text = '+', command = lambda: input_into_entry('+', '+'))
-button_plus.place(x = 250, y = 200, width = 50, height = 50)
+button_plus.place(x = 160, y = 200, width = 50, height = 50)
 
 button_equal = Button(window, bg = 'black', fg = 'white', text = '=', command = count_result)
-button_equal.place(x = 250, y = 250, width = 50, height = 50)
+button_equal.place(x = 160, y = 250, width = 50, height = 50)
 
 button_minus = Button(window, bg = 'black', fg = 'white', text = '-', command = lambda: input_into_entry('-', '-'))
-button_minus.place(x = 250, y = 150, width = 50, height = 50)
+button_minus.place(x = 160, y = 150, width = 50, height = 50)
 
 button_times = Button(window, bg = 'black', fg = 'white', text = 'x', command = lambda: input_into_entry('x', 'x'))
-button_times.place(x = 250, y = 100, width = 50, height = 50)
+button_times.place(x = 160, y = 100, width = 50, height = 50)
 
 button_div = Button(window, bg = 'black', fg = 'white', text = '/', command = lambda: input_into_entry('/', '/'))
-button_div.place(x = 250, y = 50, width = 50, height = 50)
+button_div.place(x = 160, y = 50, width = 50, height = 50)
 
 button_square = Button(window, bg = 'black', fg = 'white', text = 'x^2', command = lambda: input_into_entry('^2', 'sqr'))
-button_square.place(x = 200, y = 50, width = 50, height = 50)
+button_square.place(x = 110, y = 50, width = 50, height = 50)
 
 button_squareroot = Button(window, bg = 'black', fg = 'white', text = 'x^(1/2)', command = lambda: input_into_entry('^(1/2)', 'sqrt'))
-button_squareroot.place(x = 150, y = 50, width = 50, height = 50)
+button_squareroot.place(x = 60, y = 50, width = 50, height = 50)
 
 button_recip = Button(window, bg = 'black', fg = 'white', text = '1/x', command = lambda: input_into_entry('^(-1)', 'recip'))
-button_recip.place(x = 100, y = 50, width = 50, height = 50)
+button_recip.place(x = 10, y = 50, width = 50, height = 50)
 
 window.mainloop()
